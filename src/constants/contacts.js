5a49573cbaf9c5f3397ba5d6ee7ef0f209563ee6
EZH1809 <145156906+EZH1809@users.noreply.github.com>
2024-06-07 19:14:01 +0300

    add PATH_DB

--- src/constants/contacts.js
+++ src/constants/contacts.js
@@ -1,1 +1,4 @@
-export const PATH_DB =
+import path from 'node:path';
+const pathToWorkDir = path.join(process.cwd());
+const pathToFile = path.join(pathToWorkDir, 'src', 'db', 'db.json');
+export const PATH_DB = pathToFile;
