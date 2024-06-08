e97cf7d24ca93fa66f4917159bf933342b8aaa60
EZH1809 <145156906+EZH1809@users.noreply.github.com>
2024-06-08 20:08:22 +0300

    add thanos.js

--- src/scripts/thanos.js
+++ src/scripts/thanos.js
@@ -1,5 +1,23 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const thanos = async () => {};
+export const thanos = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    const updatedContacts = contacts.filter(() => Math.random() >= 0.5);
+
+    await fs.writeFile(PATH_DB, JSON.stringify(updatedContacts, null, 2));
+
+    console.log(
+      `Thanos has snapped his fingers. ${
+        contacts.length - updatedContacts.length
+      } contacts have been removed.`,
+    );
+  } catch (error) {
+    console.error(''Deletion Error:', error);
+  }
+};
 
 await thanos();
