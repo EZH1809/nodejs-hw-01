28fd5c238a23613b0f7c89a80f7ddaf6635d96d7
EZH1809 <145156906+EZH1809@users.noreply.github.com>
2024-06-08 11:59:36 +0300

    add generateContacts.js

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,26 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact.js';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+  try {
+    // Читання існуючого масиву контактів з файлу db.json
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = JSON.parse(data);
+
+    // Створення нових контактів
+    for (let item = 0; item < number; item++) {
+      const newContact = createFakeContact();
+      contacts.push(newContact);
+    }
+
+    // Запис оновленого масиву контактів назад у файл db.json
+    await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
+
+    console.log(`${number} new contacts have been added successfully.`);
+  } catch (error) {
+    console.error('Error generating contacts:', error);
+  }
+};
 
 await generateContacts(5);
