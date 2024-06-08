161a2860bef58593fdb2eb00ad83e3643e99bef5
EZH1809 <145156906+EZH1809@users.noreply.github.com>
2024-06-08 18:14:43 +0300

    add getAllContacts.js

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -4,17 +4,14 @@ import { createFakeContact } from '../utils/createFakeContact.js';
 
 const generateContacts = async (number) => {
   try {
-    // Читання існуючого масиву контактів з файлу db.json
     const data = await fs.readFile(PATH_DB, 'utf-8');
     const contacts = JSON.parse(data);
 
-    // Створення нових контактів
     for (let item = 0; item < number; item++) {
       const newContact = createFakeContact();
       contacts.push(newContact);
     }
 
-    // Запис оновленого масиву контактів назад у файл db.json
     await fs.writeFile(PATH_DB, JSON.stringify(contacts, null, 2));
 
     console.log(`${number} new contacts have been added successfully.`);
